8b447150cbff53219b6dfb1562ca4037febb0b53
ehsan <danialgto@gmail.com>
2024-02-14 18:27:16 +0330

    cores addded

--- core/settings.py
+++ core/settings.py
@@ -51,7 +51,8 @@ CORS_ALLOWED_ORIGINS = [
 "http://localhost:5173",
 "http://localhost:3000",
 "http://127.0.0.1:3000",
-"http://127.0.0.1:5173"
+"http://127.0.0.1:5173",
+"http://3.128.254.241"
 ]
 
 SWAGGER_SETTINGS = {
