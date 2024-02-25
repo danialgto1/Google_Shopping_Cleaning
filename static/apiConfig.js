76eb369499e3ba0c04cc70a22722321914c75492
ehsan <danialgto@gmail.com>
2024-02-25 23:22:45 +0330

    edit confident show

--- static/apiConfig.js
+++ static/apiConfig.js
@@ -3,5 +3,5 @@
 // apiConfig.js
 export const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "http://3.128.254.241.com/"
+    ? "http://3.128.254.241/"
     : "http://localhost:8000/";
