adc4d27199ce2da5641c3b442183eccb662ea050
ehsan <danialgto@gmail.com>
2024-03-13 00:32:40 +0330

    adding protocol to uri

--- home/serializers.py
+++ home/serializers.py
@@ -11,7 +11,7 @@ class ResponseDataSerializer(serializers.ModelSerializer):
     
     def get_integrated_website(self , obj):
         request = self.context.get('request')
-        print (request.META)
+        print ("======",request.is_secure())
         if request is not None:
             if 'HTTP_X_FORWARDED_PROTO' in request.META:
                 protocol = request.META['HTTP_X_FORWARDED_PROTO']
